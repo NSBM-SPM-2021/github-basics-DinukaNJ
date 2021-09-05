2dae8adfcb201237c7a78020d24f81aac8648ce2
Dinuka_NJ <mdnjayarathna@students.nsbm.ac.lk>
2021-09-05 12:33:03 +0530

    Router update

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,12 +1,13 @@
 import express from "express";
-  
 import { showTutorials } from "../controllers/tutorial.controller.js";
   
 const router = express.Router();
 
 //routes
-
-
 router.get('/', showTutorials);
+
+
+
+
 
 export default router;
